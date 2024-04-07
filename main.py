8d3f1af0e256b50b8b81ed67b927fe9a098e376b
Tobias <test@test.com>
2024-04-07 11:21:29 +0200

    Um GUI erweitert

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import custom_module
+import tkinter as tk
 
-custom_module.say_hi("Tobi")
+def say_hi (name):
+    print(f"Hallo herzlich Willkommen {name}")
+
+fenster = tk.Tk()
+fenster.geometry("100x100")
+  
+B = tk.Button(fenster, text ="Drücken", command=lambda: custom_module.say_hi("Tobi"))#Lambda wir dbenötigt für Funktionen mit Parameter
+B.pack()
+fenster.mainloop()
