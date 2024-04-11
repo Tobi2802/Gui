680d370fab903d92f9a8f56b7ad4e8cb9d301095
Meutger <163406989+Meutger@users.noreply.github.com>
2024-04-11 08:31:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import custom_module
 import tkinter as tk
 
-def say_hi (name):
-    print(f"Hallo herzlich Willkommen {name}")
 
 fenster = tk.Tk()
 fenster.geometry("100x100")
-  
-B = tk.Button(fenster, text ="Drücken", command=lambda: custom_module.say_hi("Tobi"))#Lambda wir dbenötigt für Funktionen mit Parameter
-B.pack()
+
+B1 = tk.Button(fenster, text ="Freundlich", command=lambda: custom_module.Freundliche_Nachricht("Tobi"))#Lambda wird benötigt für Funktionen mit Parameter
+B1.pack()
+ 
+B2 = tk.Button(fenster, text ="Unfreundlich", command=lambda: custom_module.Unfreundliche_Nachricht("Tobi"))#Lambda wird benötigt für Funktionen mit Parameter
+B2.pack()
 fenster.mainloop()
